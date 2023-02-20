e010d364af253eb2c911c93e28741e7e61f09579
CarlaCGDM <nccardillo@iesrafaelalberti.es>
2023-02-20 12:40:56 +0100

    limpieza

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import { fabric } from 'fabric'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
